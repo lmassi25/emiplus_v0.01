d1819abd664c7c6e86d460a8d91df49ebe55d285
lmassi25 <50420128+lmassi25@users.noreply.github.com>
2019-09-06 12:18:01 -0300

    E agora?

---
+++ Emiplus/Emiplus/Model/Imposto.cs
@@ -0,0 +1,45 @@
+﻿namespace Emiplus.Model
+{
+    using System;
+    using SqlKata;
+    using Data.Database;
+
+    class Imposto : Model
+    {
+        public Imposto() : base("IMPOSTO")
+        {
+
+        }
+
+        #region CAMPOS 
+
+        //campos obrigatorios para todas as tabelas
+
+        [Ignore]
+        [Key("ID")]
+        public int Id { get; set; }
+        public int Tipo { get; set; }
+        public int Excluir { get; set; }
+        public DateTime Criado { get; private set; }
+        public DateTime Atualizado { get; private set; }
+        public DateTime Deletado { get; private set; }
+        public string EmpresaId { get; private set; }
+
+        public string Nome { get; set; }
+
+        public string Icms { get; set; } // CST CSOSN   
+        public double IcmsReducaoAliq { get; set; }
+        public double IcmsAliq { get; set; }
+        public double IcmsStBase { get; set; }
+        public double IcmsStReducaoAliq { get; set; }
+        public double IcmsStAliq { get; set; }
+        public string Ipi { get; set; } // CST
+        public double IpiAliq { get; set; }
+        public string Pis { get; set; } // CST
+        public double PisAliq { get; set; }
+        public string Cofins { get; set; } // CST
+        public double CofinsAliq { get; set; }
+
+        #endregion
+    }
+}
