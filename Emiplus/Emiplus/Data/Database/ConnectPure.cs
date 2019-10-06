1f3737b5c7e679e76f9aa6415a4d34882b52625c
William Alvares <william@uilia.com.br>
2019-10-06 18:28:01 -0300

    Connect Pure

--- Emiplus/Emiplus/Data/Database/ConnectPure.cs
+++ Emiplus/Emiplus/Data/Database/ConnectPure.cs
@@ -1,12 +1,7 @@
 ﻿using FirebirdSql.Data.FirebirdClient;
 using Emiplus.Data.Helpers;
-using SqlKata.Compilers;
-using SqlKata.Execution;
-using System.IO;
 using Emiplus.Data.Core;
-using System.Data;
 using System;
-using System.Windows.Forms;
 
 namespace Emiplus.Data.Database
 {
@@ -18,67 +13,25 @@ namespace Emiplus.Data.Database
         private const string _db = "sysdba";
         private const string _host = "localhost";
 
-        private static FbConnection conn;
-
-
-        private FbConnection SQLCon;
-
-        private ConnectPure() { }
+        protected ConnectPure() { }
 
         private static string GetDatabase()
         {
             return _path = IniFile.Read("PathDatabase", "LOCAL") + "\\EMIPLUS.FDB";
         }
 
-        public void Connect()
-        {
-            //FbConnection SQLCon = new FbConnection(
-            //    $"character set=NONE;initial catalog=C:\\Emiplus\\EMIPLUS.fdb;user id={_user};data source={_host};user id={_db};Password={_pass};Pooling=true;Dialect=3"
-            //);
-
-            //FbCommand cmd;
-            //FbDataReader res;
-            //cmd = new FbCommand(, SQLCon);
-            //cmd.ExecuteNonQuery();
-
-            //ExecuteReader
-        }
-
-        //public void Open()
-        //{
-        //    SQLCon.Open();
-        //    //Log Log = new Log();
-        //    //Log.Add("Connect", "Path: " + GetDatabase(), Log.LogType.fatal);
-
-        //    //var connection = new FbConnection(
-        //    //    $"character set=NONE;initial catalog={GetDatabase()};user id={_user};data source={_host};user id={_db};Password={_pass};Pooling=true;Dialect=3"
-        //    //);
-
-        //    //var compiler = new FirebirdCompiler();
-        //    //var db = new QueryFactory(connection, compiler);
-
-        //    //db.Logger = compiled =>
-        //    //{
-        //    //    Log.Add("LOGGER", "Query: " + compiled.ToString(), Log.LogType.fatal);
-
-        //    //    System.Console.WriteLine(compiled.ToString());
-        //    //};
-
-        //    //return db;
-        //}
-
         /// <summary>
         /// Método responsável por abrir a conexão com Banco de Dados
         /// </summary>
-        private static FbConnection Connection()
+        protected static FbConnection Connection()
         {
             FbConnection conexao = null;
 
             try
             {
                 conexao = new FbConnection(
-                $"character set=NONE;initial catalog={GetDatabase()};user id={_user};data source={_host};user id={_db};Password={_pass};Pooling=true;Dialect=3"
-            );
+                    $"character set=NONE;initial catalog={GetDatabase()};user id={_user};data source={_host};user id={_db};Password={_pass};Pooling=true;Dialect=3"
+                );
 
                 return conexao;
             }
@@ -90,49 +43,5 @@ namespace Emiplus.Data.Database
 
             return conexao;
         }
-
-        /// <summary>
-        /// Begin Transaction: esse comando, como o nome sugere, inicia a transação, abrindo o bloco de comandos a serem executados. 
-        /// Todas as instruções que precisem ser executadas devem estar após esse comando.
-        /// </summary>
-        public static void Open()
-        {
-            if (Connection().State == ConnectionState.Closed)
-            {
-                conn = Connection();
-                conn.Open();
-            }
-
-            conn.BeginTransaction();
-        }
-
-        public static FbConnection Get()
-        {
-            return conn;
-        }
-
-        /// <summary>
-        /// Commit Transaction: o comando commit efetiva a transação, ou seja, persiste no banco todas as alterações efetuadas no bloco. 
-        /// Após a execução do commit, não é possível reverter as modificações sofridas pelos dados na base.
-        /// </summary>
-        public static void Close()
-        {
-            if (conn.State == ConnectionState.Open)
-                conn.Close();
-
-            conn.BeginTransaction().Commit();
-            conn = null;
-        }
-
-        /// <summary>
-        /// Rollback Transaction: contrário ao commit, o rollback cancela a transação. 
-        /// Assim, todos os comandos executados no bloco da transação são descartados e a base de dados não sofre nenhuma alteração. 
-        /// Esse comando é geralmente utilizado caso ocorra algum erro na execução de uma das instruções do bloco.
-        /// </summary>
-        public static void RollBack()
-        {
-            conn.BeginTransaction().Rollback();
-        }
-
     }
 }
