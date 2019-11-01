47d9520f550f55eb8b60012007015b6902e4a63e
William Alvares <curruwilla@gmail.com>
2019-11-01 09:27:41 -0300

    reports

--- Emiplus/Emiplus/View/Reports/Form2.cs
+++ Emiplus/Emiplus/View/Reports/Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +20,7 @@ namespace Emiplus.View.Reports
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            reportViewer1.LocalReport.SetParameters(new ReportParameter("parameter2", "William"));
             this.reportViewer1.RefreshReport();
         }
     }
